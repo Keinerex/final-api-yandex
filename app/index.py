a5b1a00516a5df85f40fb01433e78ddafefa5e1f
Юрков Владислав <65080284+Keinerex@users.noreply.github.com>
2023-04-19 22:33:06 +0700

    cors

--- app/index.py
+++ app/index.py
@@ -4,12 +4,19 @@ from flask import Flask
 import models
 from database import db
 from api import bp
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL",
                                                   r"postgresql://postgres:Pa$$word@localhost:5432/base")
 app.config["JSON_AS_ASCII"] = False
+
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 db.init_app(app)
+
+
 
 with app.app_context():
     db.create_all()
