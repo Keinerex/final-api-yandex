d7b1342da170a6589f8464734d644237be04e6b8
Юрков Владислав <65080284+Keinerex@users.noreply.github.com>
2023-04-23 11:01:45 +0700

    alembic and full api

--- app/api.py
+++ app/api.py
@@ -1,12 +1,153 @@
+import datetime
+import json
 from uuid import UUID
 
 from flask import Blueprint
-
-from models import Category, Book, Genre, Author, Review, User
-from database import db
 from flask import jsonify, request
+from flask_jwt_extended import JWTManager, create_access_token, set_access_cookies, unset_jwt_cookies, jwt_required, \
+    current_user, get_jwt_identity, get_jwt
+from werkzeug.security import generate_password_hash
+
+from database import db
+from models import Category, Book, Genre, Author, Review, User
 
 bp = Blueprint('api', __name__)
+
+jwt = JWTManager()
+
+
+@jwt.user_identity_loader
+def user_identity_lookup(user):
+    return user
+
+
+@jwt.user_lookup_loader
+def user_lookup_callback(_jwt_header, jwt_data):
+    identity = jwt_data["sub"]
+    return User.query.filter_by(id=identity).one_or_none()
+
+
+@bp.after_request
+def refresh_expiring_jwts(response):
+    try:
+        exp_timestamp = get_jwt()["exp"]
+        now = datetime.datetime.now(datetime.timezone.utc)
+        target_timestamp = datetime.datetime.timestamp(now + datetime.timedelta(minutes=30))
+        if target_timestamp > exp_timestamp:
+            print("new token")
+            access_token = create_access_token(identity=get_jwt_identity())
+            set_access_cookies(response, access_token)
+        return response
+    except (RuntimeError, KeyError):
+        return response
+
+
+@bp.route("/login", methods=["POST"])
+def login():
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+    print(request.json)
+    print(username, password)
+    try:
+        user = User.query.filter_by(username=username).one_or_none()
+    except Exception as e:
+        return jsonify({"msg": "error"})
+
+    if not user or not user.check_password(password):
+        return jsonify("Wrong username or password"), 401
+
+    response = jsonify({"msg": "login successful"})
+    access_token = create_access_token(identity=user.id)
+    set_access_cookies(response, access_token)
+    return response
+
+
+@bp.route("/user_existed", methods=["GET"])
+def user_existed():
+    username = request.json.get("username", None)
+    user = User.query.filter_by(username=username).one_or_none()
+    if not user:
+        return jsonify({"status": False})
+
+    return jsonify({"status": True})
+
+
+@bp.route("/register", methods=["POST"])
+def register():
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+
+    user = User.query.filter_by(username=username).one_or_none()
+
+    if not user and username and password:
+        db.session.add(User(username=username, password_hash=generate_password_hash(password), cart=json.dumps({}),
+                            favourite=json.dumps({})))
+        db.session.commit()
+        return jsonify({"msg": "register successful"})
+
+    return jsonify({"msg": "register error"})
+
+
+@bp.route("/logout", methods=["POST"])
+def logout_with_cookies():
+    response = jsonify({"msg": "logout successful"})
+    unset_jwt_cookies(response)
+    return response
+
+
+@bp.route("/get_user_data", methods=["GET"])
+@jwt_required()
+def get_user_data():
+    return jsonify(current_user.data)
+
+
+@bp.route("/is_authorized", methods=["GET"])
+@jwt_required()
+def is_authorized():
+    return jsonify({"status": True})
+
+
+@bp.route("/update_cart", methods=["PUT"])
+@jwt_required()
+def update_cart():
+    cart = request.json.get("cart", None)
+    if cart:
+        current_user.cart = json.dumps(cart)
+        db.session.commit()
+        return jsonify({"msg": "cart was updated"})
+    return jsonify({"msg": "error"})
+
+
+@bp.route("/update_favourite", methods=["PUT"])
+@jwt_required()
+def update_favourite():
+    favourite = request.json.get("favourite", None)
+    if favourite:
+        current_user.favourite = json.dumps(favourite)
+        db.session.commit()
+        return jsonify({"msg": "favourite was updated"})
+    return jsonify({"msg": "error"})
+
+
+@bp.route("/update_userdata", methods=["PUT"])
+@jwt_required()
+def update_userdata():
+    name = request.json.get("name", None)
+    surname = request.json.get("surname", None)
+    patronymic = request.json.get("patronymic", None)
+    email = request.json.get("email", None)
+    tel = request.json.get("tel", None)
+    date = request.json.get("date", None)
+    if name and surname and email and tel and date:
+        current_user.patronymic = patronymic
+
+        current_user.surname = surname
+        current_user.email = email
+        current_user.tel = tel
+        current_user.date = date
+        db.session.commit()
+        return jsonify({"msg": "userdata was updated"})
+    return jsonify({"msg": "error"})
 
 
 @bp.route("/categories", methods=["GET"])
--- app/models/user.py
+++ app/models/user.py
@@ -1,3 +1,7 @@
+import json
+
+from werkzeug.security import generate_password_hash, check_password_hash
+
 from database import db
 from .base import BaseModel
 
@@ -5,4 +9,32 @@ from .base import BaseModel
 class User(BaseModel):
     __tablename__ = "users"
 
+    username = db.Column(db.VARCHAR(80))
     name = db.Column(db.VARCHAR(80))
+    surname = db.Column(db.VARCHAR(80))
+    patronymic = db.Column(db.VARCHAR(80))
+    email = db.Column(db.String(120), unique=True)
+    tel = db.Column(db.String(16), unique=True)
+    password_hash = db.Column(db.String(128))
+    cart = db.Column(db.Text())
+    favourite = db.Column(db.Text())
+    date = db.Column(db.VARCHAR(10))
+
+    @property
+    def data(self):
+        return {
+            "username": self.username,
+            "name": self.name,
+            "surname": self.surname,
+            "patronymic": self.patronymic,
+            "email": self.email,
+            "tel": self.tel,
+            "date": self.date,
+            "cart": json.loads(self.cart)
+        }
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
